20a32821d6df71bf883446709b6d991a72fdd2fe
Paty <102148049+Patts-03@users.noreply.github.com>
2022-10-01 17:49:58 +0200

    Update recomendador.py

--- recomendador.py
+++ recomendador.py
@@ -1,15 +1,12 @@
 import pandas as pd
-
 def extract():
     df_mov = pd.read_csv('movies.csv', encoding='latin')            
-    df_rats = pd.read_csv('ratings.csv', encoding='latin')
 
-    return df_mov,df_rats
+    return df_mov
 
-def transform(df_mov,df_rats,wanted):
+def transform(df_mov,wanted):
     df_title = df_mov['title']
     df_genre = df_mov['genres']
-    df_ratings = df_rats['rating']
 
     movie_index=[]
     for a in range(len(df_genre)):
@@ -26,24 +23,24 @@ def transform(df_mov,df_rats,wanted):
         if wanted in options:
             movie_index.append(a)
 
-    return movie_index,df_title,df_ratings
+    return movie_index,df_title, df_genre
 
-def load(movie_index,df_title,df_ratings):
+def load(movie_index,df_genres,df_title):
 
     num = int(input('\nHow many films would you like to be recommended? (Introduce 0 if you want all our results) : '))
     if (len(movie_index)-1) < num:
-        print("\nSorry, we've just found",len(movie_index)-1," coincidences")
+        print("\nSorry, we've just found",len(movie_index)-1,"coincidences")
         num = len(movie_index)-1
     elif num == 0:
         num = len(movie_index)-1
 
     print("\nBased on your initial choice, we are sure you'll love these films")
-      
+    
     for a in range(num):
         index = movie_index[a]
         title = df_title[index]
-        rate = df_ratings[index]
-        print('\n- Title:', title,'\n ---> Rated:',rate)
+        genre = df_genres[index]
+        print('\n- Title:', title,'\n ---> Complete genre:',genre)
 
     print('\n -- We hope you enjoy our recommendation --\n')
 
@@ -52,11 +49,11 @@ def load(movie_index,df_title,df_ratings):
 
 if __name__=='__main__':
     valid = False
-    genres_list = ['Action','Adventure','Animation', "Childrend's", 'Comedy' , 'Crime', 'Documentary','Drama','Fantasy','Film-Noir','Horror','Musical','Mystery','Romance','Sci-Fi','Thriller','War', 'Western']
+    genres_list = ['Action','Adventure','Animation',"Childrend's",'Comedy','Crime','Documentary','Drama','Fantasy','Film-Noir','Horror','Musical','Mystery','Romance','Sci-Fi','Thriller','War','Western']
     
     # Inicio el programa y hago un print de los géneros
     print('\nWelcome to your movie recommendator!\n''Please introduce one of the following genres in order to be recommended a new film:\n ')
-    for genre in genres_list:
+    for genre in genres_list:  
         print('-',genre)
     
     # Obtenemos el género base y ponemos en marcha la búsqueda
@@ -68,11 +65,12 @@ if __name__=='__main__':
             wanted = input('\nYour choice: ')
         else:
             valid = True
-    print("Now we'll search in out database. Please be patient")
+    print("Now we'll search in our database. Please be patient")
 
-    df_mov,df_rats = extract()
-    movie_index,df_title,df_ratings = transform(df_mov,df_rats,wanted)
-    load(movie_index,df_title,df_ratings)
+    df_mov = extract()
+    movie_index,df_title,df_genres = transform(df_mov,wanted)
+    load(movie_index,df_genres,df_title)
+
 
 
 
